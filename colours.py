0f834880d248847906e6521e57d3b1bda465a3b7
theminotaur <mxpen26@gmail.com>
2014-04-04 17:26:04 -0700

    a redo of most things, with more separate files
    who here likes bugs?

---
+++ colours.py
@@ -0,0 +1,25 @@
+def testIsWhite(RGBA):
+	pxIsWhite = True
+	RGB = RGBA[:3]
+
+	RGBl = 255
+	RGBu = 0
+
+	RGBsum = 0
+
+	for colour in RGB:
+		if(RGBl > colour):
+			RGBl = colour
+		
+		if(RGBu < colour):
+			RGBu = colour
+	
+		RGBsum = RGBsum + colour
+	
+	RGBrange = RGBu - RGBl
+	RGBmean = RGBsum / 3 
+
+	if(RGBrange > 10 or RGBmean < 145):
+		pxIsWhite = False
+
+	return pxIsWhite
---
+++ lines.py
@@ -0,0 +1,43 @@
+class SimpleImage:
+	def __init__(self, PILimage):
+		self.height = PILimage.size[1]
+		self.width = PILimage.size[0]
+		self.pixels = list(PILimage.getdata())
+
+class LineSpaceFinder:
+	def __init__(self, seed, condition, image):
+		self.im = SimpleImage(image)
+		self.seed = seed
+		self.condition = condition
+	
+	def perform(self):
+		print 'performing...'
+		self.upLine = Line(0,self.seed,self.condition, self.im)	
+
+class Line:
+	def __init__(self, direction, seed, condition, image):
+		self.direction = direction
+		self.seed = seed
+		self.condition = condition
+		self.im = image
+
+		translations = [
+			lambda seed: seed + self.im.width,
+			lambda seed: seed + 1,
+			lambda seed: seed - self.im.width,
+			lambda seed: seed - 1
+		]
+		lineLen = 0
+		pxSatisfies = False
+		currentPx = seed
+
+		while pxSatisfies:
+			lineLen = lineLen + 1
+			currentPx = translations[direction](seed)
+
+			pxSatisfies = condition(self.im.pixels[currentPx])
+			print self.im.pixels[currentPx]
+			print lineLen
+
+		self.length = lineLen
+
